050cd8128f8c439a5d9fd064488ca2d8b3d9f3e5
deep2web <88986286+deep2web@users.noreply.github.com>
2024-09-13 15:04:54 +0200

    -

--- steuerung_relay.py
+++ steuerung_relay.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 while True:
     GPIO.output(23, GPIO.HIGH)
     GPIO.output(18, GPIO.HIGH)
